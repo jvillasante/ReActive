e0f8d49f27f1745300d8a4b429c6a77b96a9be05
Julio Cesar Villasante <jvillasantegomez@gmail.com>
2015-01-15 18:11:15 -0300

    creacion de reporte

--- data/reportProvider.js
+++ data/reportProvider.js
@@ -30,25 +30,70 @@ ReportProvider.prototype.createNew = function(userId, projectId, templateId, cal
         done(client); 
         return callback(Err("no such template", { code: 404, description: "Template " + templateId + " not found for project " + projectId, errors: []}));
       }
-      
-      sql = [];
-      sql.push("INSERT INTO reports(id_user, id_project, id_template, title)");
-      sql.push("VALUES($1, $2, $3, $4) RETURNING id");
-      client.query(sql.join(' '), [userId, projectId, template.id, template.title], function(err, result) {
-        if (err) { 
-          done(client);
-          return callback(Err("db query error", { code: 1002, description: err.message, errors: []}));
+
+      async.waterfall([
+        function createReport(next) {
+          sql = [];
+          sql.push("INSERT INTO reports(id_user, id_project, id_template, title)");
+          sql.push("VALUES($1, $2, $3, $4) RETURNING id");
+          client.query(sql.join(' '), [userId, projectId, template.id, template.title], function(err, result) {
+            if (err) { return next(Err("db query error", { code: 1002, description: err.message, errors: []})); }
+            next(null, result.rows[0].id);
+          });
+        }, 
+        function createReportData(reportId, next) {
+          let resultData = _.cloneDeep(template);
+          resultData.data.id = reportId;
+          let sections = template.data.sections;
+
+          async.each(sections, function(section, cb) {
+            let sectionsCb = cb, subsections = section.subsections;
+            client.query("INSERT INTO sections(id_report, name) VALUES($1, $2) RETURNING id", [reportId, section.name], function(err, result) {
+              if (err) { return cb(Err("db query error", { code: 1002, description: err.message, errors: []})); }
+              let sectionId = result.rows[0].id;
+              resultData.data.sections[section.id - 1].id = sectionId;
+              async.each(subsections, function(subsection, cb) {
+                let subsectionsCb = cb, groups = subsection.groups;
+                client.query("INSERT INTO subsections(id_section, name) VALUES($1, $2) RETURNING id", [sectionId, subsection.name], function(err, result) {
+                  if (err) { return cb(Err("db query error", { code: 1002, description: err.message, errors: []})); }
+                  let subsectionId = result.rows[0].id;
+                  resultData.data.sections[section.id - 1].subsections[subsection.id - 1].id = subsectionId;
+                  async.each(groups, function(group, cb) {
+                    let groupsCb = cb, fields = group.fields;
+                    client.query("INSERT INTO groups(id_subsection, name, comment) VALUES($1, $2, $3) RETURNING id", [subsectionId, group.name, group.comment], function(err, result) {
+                      if (err) { return cb(Err("db query error", { code: 1002, description: err.message, errors: []})); }
+                      let groupId = result.rows[0].id;
+                      resultData.data.sections[section.id - 1].subsections[subsection.id - 1].groups[group.id - 1].id = groupId;
+                      async.each(fields, function(field, cb) {
+                        client.query("INSERT INTO fields(id_group, name) VALUES($1, $2) RETURNING id", [groupId, field.name], function(err, result) {
+                          if (err) { return cb(Err("db query error", { code: 1002, description: err.message, errors: []})); }
+                          let fieldId = result.rows[0].id;
+                          resultData.data.sections[section.id - 1].subsections[subsection.id - 1].groups[group.id - 1].fields[field.id - 1].id = fieldId;
+                          cb();
+                        });
+                      }, function(err) {
+                        if (err) { return next(err); }   
+                        groupsCb();
+                      });
+                    });
+                  }, function(err) {
+                    if (err) { return next(err); }
+                    subsectionsCb();
+                  });
+                });
+              }, function(err) {
+                if (err) { return next(err); }
+                sectionsCb();
+              });
+            });
+          }, function(err) {
+            if (err) { return next(err); }
+            next(null, resultData.data);
+          });
         }
-        
-        let report = result.rows[0];
-        if (!report) {
-          done(client); 
-          return callback(Err("error creating report", { code: 500, description: "Cannot create report for template " + templateId, errors: []}));
-        }
-        
-        done();
-        template.data.id = report.id;
-        callback(null, template.data);
+      ], function(err, result) {
+        if (err) { done(client); return callback(err); }
+        callback(null, result);
       });
     });
   });
