a0ff67aae878f2c20e78a7248ee4de5ed57143e3
Julio Cesar Villasante <jvillasantegomez@gmail.com>
2015-02-03 17:09:52 -0300

    borrando antiguio proyecto para consolidacion del api y web en un solo repo

--- migrations/sqls/20150118175514-add-tbl-fields-up.sql
+++
@@ -1,1 +0,0 @@
-CREATE TABLE fields (  id UUID PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),  id_report UUID NOT NULL,    FOREIGN KEY (id_report) REFERENCES reports (id)     ON UPDATE CASCADE    ON DELETE CASCADE);
--- migrations/sqls/20150118175517-add-tbl-values-up.sql
+++
@@ -1,1 +0,0 @@
-CREATE TABLE values (  id_field UUID NOT NULL,  item INT NOT NULL,  name VARCHAR(255) NOT NULL,  value TEXT,    PRIMARY KEY (id_field, item),  FOREIGN KEY (id_field) REFERENCES fields (id)     ON UPDATE CASCADE    ON DELETE CASCADE);
