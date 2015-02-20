d7d7ff06be7de8c6bccfc254c78a28e61548c31b
Julio Cesar Villasante <jvillasantegomez@gmail.com>
2015-02-20 17:59:45 -0300

    comienza desarrollo para graficos

--- web/src/components/Dashboard/Graphics.jsx
+++ web/src/components/Dashboard/Graphics.jsx
@@ -2,8 +2,42 @@
 
 var React = require('react');
 var ReactBootstrap = require('react-bootstrap');
+var OverlayMixin = ReactBootstrap.OverlayMixin;
+var Modal = ReactBootstrap.Modal;
+var Button = ReactBootstrap.Button;
 
 var Graphics = React.createClass({
+  mixins: [OverlayMixin],
+
+  getInitialState: function() {
+    return {
+      isModalOpen: false
+    };
+  },
+
+  handleToggle: function(evt) {
+    evt.preventDefault();
+
+    this.setState({
+      isModalOpen: !this.state.isModalOpen
+    });
+  },
+
+  renderOverlay: function() {
+    if (!this.state.isModalOpen) { return <span/>; }
+
+    return (
+      <Modal title="Modal heading" onRequestHide={this.handleToggle}>
+        <div className="modal-body">
+          This modal is controlled by our custom trigger component.
+        </div>
+        <div className="modal-footer">
+          <Button onClick={this.handleToggle}>Close</Button>
+        </div>
+      </Modal>
+    );
+  },
+
   render: function() {
     return (
       <div className="container dashboard-graphics">
@@ -15,80 +49,10 @@ var Graphics = React.createClass({
                   <span className="glyphicon glyphicon-th"></span>
                 </div>
                 <div className="card-title">
-                  Nombre de Gráfico
+                  Sistema Last Planner
                 </div>
               </div>
-              <img className="img-responsive img-rounded" src="images/s01.jpg" alt="" />
-            </div>
-          </div>
-
-          <div className="col-md-4">
-            <div className="card-charts small-card">
-              <div className="main-card-header">
-                <div className="icon-card-header">
-                  <span className="glyphicon glyphicon-th"></span>
-                </div>
-                <div className="card-title">
-                  Nombre de Gráfico
-                </div>
-              </div>
-              <img className="img-responsive img-rounded" src="images/s02.jpg" alt="" />
-            </div>
-          </div>
-
-          <div className="col-md-4">
-            <div className="card-charts small-card">
-              <div className="main-card-header">
-                <div className="icon-card-header">
-                  <span className="glyphicon glyphicon-th"></span>
-                </div>
-                <div className="card-title">
-                  Nombre de Gráfico
-                </div>
-              </div>
-              <img className="img-responsive img-rounded" src="images/s03.jpg" alt="" />
-            </div>
-          </div>
-
-          <div className="col-md-4">
-            <div className="card-charts small-card">
-              <div className="main-card-header">
-                <div className="icon-card-header">
-                  <span className="glyphicon glyphicon-th"></span>
-                </div>
-                <div className="card-title">
-                  Nombre de Gráfico
-                </div>
-              </div>
-              <img className="img-responsive img-rounded" src="images/s04.jpg" alt="" />
-            </div>
-          </div>
-
-          <div className="col-md-4">
-            <div className="card-charts small-card">
-              <div className="main-card-header">
-                <div className="icon-card-header">
-                  <span className="glyphicon glyphicon-th"></span>
-                </div>
-                <div className="card-title">
-                  Nombre de Gráfico
-                </div>
-              </div>
-              <img className="img-responsive img-rounded" src="images/s05.jpg" alt="" />
-            </div>
-          </div>
-
-          <div className="col-md-4">
-            <div className="card-charts small-card">
-              <div className="main-card-header">
-                <div className="icon-card-header">
-                  <span className="glyphicon glyphicon-th"></span>
-                </div>
-                <div className="card-title">
-                  Nombre de Gráfico
-                </div>
-              </div>
-              <img className="img-responsive img-rounded" src="images/s06.jpg" alt="" />
+              <a href="#" onClick={this.handleToggle}><img className="img-responsive img-rounded" src="images/s05.jpg" alt="" /></a>
             </div>
           </div>
         </div>
