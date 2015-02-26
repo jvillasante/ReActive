376d62b455a3b584d21e31a63c27e1836266c6e3
Julio Cesar Villasante <jvillasantegomez@gmail.com>
2015-02-26 18:29:19 -0300

    cambios ligeros...

--- web/src/components/Dashboard/BenchmarkTable.jsx
+++ web/src/components/Dashboard/BenchmarkTable.jsx
@@ -15,7 +15,7 @@ var BenchmarkTable = React.createClass({
             </div>
           </div>
 
-          <div className="col-md-3">
+          <div className="col-md-5">
             <h3 className="sub-header">Proyectos</h3>
             <div className="table-responsive">
               <table className="table table-condensed projects-table table-header-rotated">
@@ -32,7 +32,7 @@ var BenchmarkTable = React.createClass({
               </table>
             </div>
           </div>
-          <div className="col-md-9">
+          <div className="col-md-7">
             <h3 className="sub-header">Consolidado de Indicadores</h3>
             <div className="table-responsive">
             <table className="table table-condensed table-header-rotated general-data-table">
