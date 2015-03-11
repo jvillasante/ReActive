ea1f83eed3f6c553b2a0d61d538ad1166978ea5e
Julio Cesar Villasante <jvillasantegomez@gmail.com>
2015-03-11 18:07:58 -0300

    avanza en la nueva funcionalidad

--- web/src/components/Reports/Reports.jsx
+++ web/src/components/Reports/Reports.jsx
@@ -35,42 +35,44 @@ var Reports = React.createClass({
 
   render: function() {
     return (
-      <div className="container">
-        <div className="row">
-          <Panel header={<h1>{this.state.project}</h1>}>
-            <div className="project-input-daterange col-md-12">
-              <p>Documento: (Sistema Last Planner o lo que sea)</p>
-              <p>Seccion: (Participacion en Reunion o lo que sea)</p>
-              <p>Rango de Fechas: (El rango de fechas segun marcado en el dashboard)</p>
-            </div>
-          </Panel>
+      <div className="project-input">
+        <Panel header={<h1>{this.state.project}</h1>}>
+          <div className="project-input-daterange col-md-12">
+            <p>Documento: (Sistema Last Planner o lo que sea)</p>
+            <p>Seccion: (Participacion en Reunion o lo que sea)</p>
+            <p>Rango de Fechas: (El rango de fechas segun marcado en el dashboard)</p>
+          </div>
+        </Panel>
+        <div className="container">
+          <div className="row">
 
-          {this.state.data.map(function(row, rowIndex) {
-            return (
-              <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
-                <div className="box">
-                  <span className="pull-right">(Nombre del usuario que lo creo)</span>
-                  <span className="pull-left">(Ultima actualizacion del reporte)</span>
-                  <div className="box-icon">
-                    <span><img className="img-responsive img-circle" src={"http://reactive.innobis.cl/" + row.userImage} alt="Test Image" /></span>
-                  </div>
-                  <div className="info">
-                    <h4 className="text-center">{row.title}</h4>
-                    <ul className="list-group">
-                      {row.data.map(function(row, rowIndex) {
-                        return (
-                          <li className="list-group-item">
-                            <span className="badge">{row.answer}</span>
-                            {row.question}
-                          </li>
-                        );
-                      })}
-                    </ul>
+            {this.state.data.map(function(row, rowIndex) {
+              return (
+                <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                  <div className="box">
+                    <span className="pull-right">(Nombre del usuario que lo creo)</span>
+                    <span className="pull-left">(Ultima actualizacion del reporte)</span>
+                    <div className="box-icon">
+                      <span><img className="img-responsive img-circle" src={"http://reactive.innobis.cl/" + row.userImage} alt="User Image" /></span>
+                    </div>
+                    <div className="info">
+                      <h4 className="text-center">{row.title}</h4>
+                      <ul className="list-group">
+                        {row.data.map(function(row, rowIndex) {
+                          return (
+                            <li className="list-group-item">
+                              <span className="badge">{row.answer}</span>
+                              {row.question}
+                            </li>
+                          );
+                        })}
+                      </ul>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+        </div>
       </div>
     </div>
     );
